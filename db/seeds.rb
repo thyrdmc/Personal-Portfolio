eaa7b6f46b351ef93333bc73ebb9b2e99a9bb27e
thyrdmc <mtyardimci45@gmail.com>
2023-03-26 05:53:37 +0300

    Created Seeds.rb file for Sample Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+10.times do |blog|
+    Blog.create!(
+        title:'My Blog Post #{blog}',
+        body: 'ádad sdgfdgnr bfdbd',
+    )
+end
+
+puts 'Ten Blog Posts Created'
+
+5.times do |skill|
+    Skill.create!(
+        title:'Rails #{skill}',
+        percent_utilizied: 15,
+    )
+end
+
+puts 'Five Skills Created'
+
+5.times do |portfolio_item|
+    Portfolio.create!(
+        title:'Portfolio title #{portfolio_item}',
+        subtitle: 'My great services',
+        body: 'Lorem Ipsum',
+        main_image: 'http://placehold.it/600x400' , 
+        thumb_image: 'http://placehold.it/350x200', 
+    )
+end
+
+puts 'Nine Portfolio Item Created'
